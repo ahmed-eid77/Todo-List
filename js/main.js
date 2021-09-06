9e886b695451c8f029327dba5c579b921d0e098c
ahmed-eid77 <70501973+ahmed-eid77@users.noreply.github.com>
2021-09-06 23:21:42 +0200

    thirdcommit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//plugins 
+$("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '38px' });
+
+
+
+
+
 // Selectors
  const addSection = document.querySelector('.add-section');
  const headerSection = document.querySelector('.header-section');
@@ -7,12 +14,13 @@
  const colorButton = document.querySelector(".color-btn");
  const submitColorButton = document.querySelector(".submit-color-btn");
  const noTasks = document.querySelector(".no-tasks");
- 
+ const filter = document.querySelector(".filter");
 
  // Event Listeners
- todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteComplete);
 submitColorButton.addEventListener('click', changeColor);
+filter.addEventListener('click',filterList)
 
  // Fuctions
  function addTodo(event) {
@@ -30,7 +38,7 @@ submitColorButton.addEventListener('click', changeColor);
      }else{
          todoDiv.style.display = "none";
          alert("Invalid Data. Try Again");
-         return;
+        return;
      }
      // Check Button
      const completedButton = document.createElement('button');
@@ -47,27 +55,31 @@ submitColorButton.addEventListener('click', changeColor);
       // clear Input Value
       todoInput.value = '';
       // delet no tasks li
-      if(todoList.childNodes.length > 1){
-        noTasks.style.display = "none"
-     }
+      noTasks.style.display = "none";
+     
  }
 
-function deleteComplete(e) {
+
+ // To delete OR completed tasks
+function deleteComplete(e,) {
     const item = e.target;
     // Delete li
-    if(item.classList[0] === 'delete-btn'){
+    if(item.classList.contains("delete-btn")){
         const todo = item.parentElement;
         todo.classList.add('fall');
         todo.addEventListener("transitionend", function(){
             todo.remove();
         })
-        //todo.remove();
+        
+        if(todoList.children.length <= 1){
+            noTasks.style.display = "flex"
+        }
     }
 
     // check li 
-    if (item.classList[0] === 'completed-btn'){
-        const todo = item.previousElementSibling;
-        todo.classList.toggle("completed");
+    if (item.classList.contains('completed-btn')){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
     }
 }
 
@@ -76,4 +88,23 @@ function changeColor(event){
     event.preventDefault();
     addSection.style.backgroundColor = colorButton.value;
     headerSection.style.backgroundColor = colorButton.value;
+}
+
+function filterList(e){
+     const todos = todoList.childNodes[0];
+     
+     todos.foreach(function(todos){
+        switch(e.target.value){
+            case "all":
+                todos.style.display = "flex"
+                break;
+            case "completed":
+                if (todos.classList.contains("completed")){
+                    todos.style.display = "flex";
+                }else{
+                    todos.style.display = "none";
+                }
+                break;
+        }
+     })
 }
